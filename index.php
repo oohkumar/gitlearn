5d88ffbd32a9ce68c25a231ea64f856ef5e26018
Kumar Desai <kumar.desai@liveramp.com>
2019-11-20 09:27:34 -0800

    my first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Git learn</title>
+</head>
+<body>
+
+	<h3>Edit One</h3>
+
+	<table>
+		<tr>
+			<td>test1</td>
+			<td>test2</td>
+			<td>test3</td>
+			<td>test4</td>
+			<td>test5</td>
+		</tr>
+	</table>
+	
+</body>
+</html>
